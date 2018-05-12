dbb794abae148f024103c0207c49ef4bba7ca32a
Marla Sowards <marla.sowards@healthcatalyst.com>
2018-05-12 11:39:01 -0600

    update

--- executions.cmd
+++ executions.cmd
@@ -10,7 +10,8 @@ move docs-project-template\resources .
 move docs-project-template\templates .
 move docs-project-template\package.json .
 move docs-project-template\readme.md .
-docker run -v %cd%:c:/docs -p 8080:8080 hcdocs/docfx:latest docfx build c:/docs/docfx.json
+timeout 2
 rmdir /s /q docs-project-template
+docker run -v %cd%:c:/docs -p 8080:8080 hcdocs/docfx:latest docfx build c:/docs/docfx.json
 cd _site
 start chrome --new-window "http://localhost:8080" && docfx serve
