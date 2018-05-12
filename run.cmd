8d6dcd4c85fa602dcfd8f6d81abcc515ea5c4d76
Marla Sowards <marla.sowards@healthcatalyst.com>
2018-05-12 11:41:05 -0600

    updates

--- run.cmd
+++ run.cmd
@@ -1,5 +1,6 @@
 git clone https://github.com/HealthCatalyst/docfx.docker.git update
 move update\run-me.cmd .
 move update\rebuild.cmd .
+move update\executions.cmd .
 rmdir /s /q update
 executions.cmd
