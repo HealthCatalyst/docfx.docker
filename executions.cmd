f9ff28a3c1041c0d9b2f915de23ad18ec5dfa872
Marla Sowards <marla.sowards@healthcatalyst.com>
2018-05-12 14:24:17 -0600

    updates

--- executions.cmd
+++ executions.cmd
@@ -1,15 +1,16 @@
 docker pull hcdocs/docfx:latest
-rmdir _site
+rmdir /s /q _site
 rmdir /s /q docs-project-template
 rmdir /s /q resources
 rmdir /s /q templates
 del package.json
 del readme.md
 git clone -b docker https://github.com/HealthCatalyst/docs-project-template.git
-move docs-project-template\resources .
-move docs-project-template\templates .
-move docs-project-template\package.json .
-move docs-project-template\readme.md .
+cd docs-project-template/resources
+robocopy docs-project-template\resources resources /E /IS /MOVE
+robocopy docs-project-template\templates templates /E /IS /MOVE
+move /y docs-project-template\package.json .
+move /y docs-project-template\readme.md .
 timeout 2
 rmdir /s /q docs-project-template
 docker run -v %cd%:c:/docs -p 8080:8080 hcdocs/docfx:latest docfx build c:/docs/docfx.json
