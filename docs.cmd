1b8057549ea62e435c26964be895f76dcd41f06a
cedar-ave <marla.sowards@healthcatalyst.com>
2018-05-14 12:35:19 -0600

    Update docs.cmd

--- docs.cmd
+++ docs.cmd
@@ -1,4 +1,5 @@
 docker pull hcdocs/docfx:latest
+npm install
 docker run -v %cd%:c:/docs -p 8080:8080 -m 2GB hcdocs/docfx:latest docfx build c:/docs/docfx.json
 cd _site
 start chrome --new-window "http://localhost:8080" && docfx serve
