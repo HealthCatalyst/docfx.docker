b2f3377ccb887fa560cb88aa7adc1daf04adbf47
cedar-ave <marla.sowards@healthcatalyst.com>
2018-05-14 12:09:13 -0600

    Create docs.cmd

---
+++ docs.cmd
@@ -0,0 +1,4 @@
+docker pull hcdocs/docfx:latest
+docker run -v %cd%:c:/docs -p 8080:8080 -m 2GB hcdocs/docfx:latest docfx build c:/docs/docfx.json
+cd _site
+start chrome --new-window "http://localhost:8080" && docfx serve
